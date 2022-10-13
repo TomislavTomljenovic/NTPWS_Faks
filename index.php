29f10e299fce7a6840a1c34bd63bf03039b7d8fa
TomislavTomljenovic <tomislavtomljenovic253@gmail.com>
2022-10-13 15:30:00 +0200

    Added footer

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta name="description" content="">
 		<meta name="keywords" content="">
-		<meta name="author" content="Alen Šimec">
+		<meta name="author" content="Tomislav Tomljenović">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-	</head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>	
+	<?php 
+	echo "Hello";
+	?>
+
 <body>
+
+<footer id="footer">Tomislav Tomljenović &#169;
+<a href="https://github.com/TomislavTomljenovic/NTPWS_Faks">Github</a>
+</footer>
 
 </body>
 </html>
